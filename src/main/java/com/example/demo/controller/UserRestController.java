1b9cd2e87b383ef9ffa4116d5a26ad1931ba993e
ReadTheWind <liuhuanasjob@163.com>
2018-04-13 17:55:54 +0800

    1

--- src/main/java/com/example/demo/controller/UserRestController.java
+++ src/main/java/com/example/demo/controller/UserRestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
-@RestController
+@RestController  
 @RequestMapping("/rest")
 public class UserRestController {
 
